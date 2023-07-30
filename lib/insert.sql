71f7c5a75c6714d1dd6c0655b4a598a1e62a73f9
Dennis Demir <dennisjdemir@gmail.com>
2023-07-30 16:42:09 -0400

    complete lab

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 8, 'M', 'White', 'Playful', True);
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 9, 'M', 'Black', 'Rowdy', True);
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 5, 'F', 'Black', 'Social', True);
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 20, 'M', 'Brown', 'Strict', True);
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 3, 'F', 'Brown', 'Playful', True);
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 10, 'M', 'Black', 'Mean', True);
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 11, 'F', 'Brown', 'Social', True);
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 2, 'F', 'Black', NULL, True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 8, 'M', 'White', 'Playful', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 9, 'M', 'Black', 'Rowdy', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 5, 'F', 'Black', 'Social', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 20, 'M', 'Brown', 'Strict', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 3, 'F', 'Brown', 'Playful', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 10, 'M', 'Black', 'Mean', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 11, 'F', 'Brown', 'Social', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 2, 'F', 'Black', NULL, 1);
